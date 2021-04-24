d531334064b10d63ee6836b16b33db2e3eb1f5f0
Kirill Klimenko <kirill0186@gmail.com>
2021-04-24 18:39:55 +0300

    Add bearer token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 const bcrypt = require('bcryptjs');
 
@@ -6,7 +7,10 @@ import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateTeacher(email: string, pass: string): Promise<any> {
     const teacher = await this.usersService.findOne(email);
@@ -18,5 +22,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.userId };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
