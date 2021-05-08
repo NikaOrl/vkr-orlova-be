842823313ed035309fb1ddfc040da21bc9fef44d
Kirill Klimenko <kirill0186@gmail.com>
2021-05-08 22:04:36 +0300

    Add request to create group with students

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,12 +1,23 @@
+import * as R from 'ramda';
+import { v4 as uuid } from 'uuid';
 import { Injectable } from '@nestjs/common';
 
 import { KnexService } from '../knex/knex.service';
 
 import { GroupDB } from './groups.interface';
+import { StudentDB } from '../students/students.interface';
+import { StudentsService } from '../students/students.service';
+
+export interface IGroupWithStudents extends GroupDB {
+  students: StudentDB[];
+}
 
 @Injectable()
 export class GroupsService {
-  constructor(private readonly knexService: KnexService) {}
+  constructor(
+    private readonly knexService: KnexService,
+    private readonly studentsService: StudentsService,
+  ) {}
 
   async getGroups(): Promise<GroupDB[]> {
     const knex = this.knexService.getKnex();
@@ -18,5 +29,47 @@ export class GroupsService {
     const knex = this.knexService.getKnex();
 
     return knex<GroupDB>('groups').whereIn('id', ids).select('*');
+  }
+
+  async getGroupById(id: string): Promise<GroupDB> {
+    const knex = this.knexService.getKnex();
+    console.log(id);
+    const [group] = await knex<GroupDB>('groups').where('id', id).select('*');
+
+    return group;
+  }
+
+  async updateGroup(id: string, groupData: GroupDB): Promise<void> {
+    const knex = this.knexService.getKnex();
+
+    await knex<GroupDB>('groups').where('id', id).update(groupData);
+  }
+
+  async createGroup(groupData: Omit<GroupDB, 'id'>): Promise<string> {
+    const knex = this.knexService.getKnex();
+
+    const id = uuid();
+
+    await knex<GroupDB>('groups').insert({
+      id,
+      ...groupData,
+    });
+
+    return id;
+  }
+
+  async createGroupWithStudents(
+    groupWithStudents: IGroupWithStudents,
+  ): Promise<void> {
+    const groupData = R.omit(['students'])(groupWithStudents);
+
+    const groupId = await this.createGroup(groupData);
+
+    const studentsToCreate = R.pipe(
+      R.prop('students'),
+      R.map(R.set(R.lensProp('groupId'), groupId)),
+    )(groupWithStudents);
+
+    await this.studentsService.createStudents(studentsToCreate);
   }
 }
